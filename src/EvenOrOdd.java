6302a19a06d11be0d959189341dd140b7d84504e
Forbesy <mvforbes21@gmail.com>
2023-06-02 23:08:17 -0400

    Completed code

--- src/EvenOrOdd.java
+++ src/EvenOrOdd.java
@@ -1,7 +1,19 @@
-public class Main
+public class EvenOrOdd
 {
     public static void main(String[] args)
     {
-       
+       int numToCheck = 5;
+
+        System.out.println("Is " + numToCheck + " even or odd?");
+
+        if(numToCheck % 2==0)
+        {
+            System.out.println(numToCheck + " is an even number.");
+        }
+        else
+        {
+            System.out.println(numToCheck + " is an odd number.");
+        }
+
     }
 }
